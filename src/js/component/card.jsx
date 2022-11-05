c38f1124b8d54a9767c62f840e726b6f715d0915
Juan David <jdda886@gmail.com>
2022-11-05 16:17:12 +0000

    corrected landing page react

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 
-const Card = () =>{
-	return(
-<div className="card m-5">
-			<img className="card-img-top" src="../../.learn/assets/Dylan.png?raw=true" alt="Card image cap" />
-			<div className="card-body">
-				<h5 className="card-title">Bob Dylan</h5>
-				<p className="card-text">
-					Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer/songwriter, author, and artist who has been an
-					influential figure in popular music and culture for more than five decades.
-				</p>
-				<a href="https://en.wikipedia.org/wiki/Bob_Dylan" className="btn btn-primary">
-					Go to wikipedia
-				</a>
-			</div>
-		</div>)
-
+const Card = (props) => {
+  return (
+    <div className="col">
+      <div className="card m-5">
+        <img className="card-img-top" src={props.image} alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">{props.title}</h5>
+          <p className="card-text">{props.text}</p>
+          <a
+            href="https://en.wikipedia.org/wiki/Bob_Dylan"
+            className="btn btn-primary"
+          >
+            {props.button}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/js/index.js
+++ src/js/index.js
@@ -14,16 +14,19 @@ import Footer from "./component/footer.jsx"
 
 //render your react application
 ReactDOM.render(
-  <div>
+  <div >
     <NavBar />
     <Jumbotron
-      title="Welcome to react"
-      description="React is the most popular rendering library in the world"
-      buttonLabel="Go to the official website"
+      title="Check THIS out!"
+      description="Did you drink water?!"
+      buttonLabel="Click to start"
       buttonURL="https://reactjs.org/"
     />
-    <Card />
-    <Footer/>
+    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+    <Card title="First title" text="Some description here" button="hello please click here" image="https://bigthink.com/wp-content/uploads/2022/08/AdobeStock_306680244.jpeg?resize=480,270"/>
+    <Card title="Random words" text="random description here, Hello!!" button="Dont click this one!" image="https://cdn-adventures-live.azureedge.net/adventurescache/e/4/c/2/4/8/e4c2482e0df3df62761d5b4cc43ba78454c92577.jpg"/>
+    <Card title="This one had more thought" text="but nothing intresting came up here" button="Click if you want" image="https://images-gmi-pmc.edge-generalmills.com/df109202-f5dd-45a1-99b4-f10939afd509.jpg"/>
+   </div> <Footer/>
   </div>,
   document.querySelector("#app")
 );
